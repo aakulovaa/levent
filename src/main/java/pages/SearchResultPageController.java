352ce8a379808fd7a01d8b1532a955f2f9315942
annakulova <aakulovaa03@gmail.com>
2024-04-07 00:40:52 +0300

    работа с страницами
    добавление страницы для отображения результатов поиска

---
+++ src/main/java/pages/SearchResultPageController.java
@@ -0,0 +1,36 @@
+package pages;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SearchResultPageController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    void initialize() {
+        backButton.setOnAction(event1 -> {
+            backButton.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("HomePage.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        });
+    }
+}
