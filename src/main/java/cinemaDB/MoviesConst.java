82f49ae5cdfddfe7304b091a2f66080a6d4899a0
annakulova <aakulovaa03@gmail.com>
2024-04-19 14:56:06 +0300

    работа с базой данных + страницы расписания

---
+++ src/main/java/cinemaDB/MoviesConst.java
@@ -0,0 +1,14 @@
+package cinemaDB;
+
+public class MoviesConst {
+    public static final String MOVIES_TABLE = "movies";
+
+    public static final String MOVIE_ID = "movie_id";
+    public static final String MOVIE_NAME = "movie_name";
+    public static final String MOVIE_YEAR_RELEASE = "movie_year_release";
+    public static final String MOVIE_DIRECTOR = "movie_director";
+    public static final String MOVIE_LENGTH = "movie_length";
+    public static final String MOVIE_AGE_LIMIT = "movie_age_limit";
+    public static final String MOVIE_GENRE = "movie_genre";
+    public static final String MOVIE_DESCRIPTION = "movie_description";
+}
